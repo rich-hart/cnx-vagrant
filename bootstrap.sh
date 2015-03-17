79e1d579068bc68b116d45f55e933bda1f51d3c0
karen chan <karen@karen-chan.com>
2015-03-17 11:16:36 +0000

    Move webview_run task after all the changes to src/scripts/settings.js

--- bootstrap.sh
+++ bootstrap.sh
@@ -76,10 +76,11 @@ virtualenv .
 sed -i "s/devarchive.cnx.org/$ipaddr/" ~/webview/src/scripts/settings.js
 sed -i 's/port: 80$/port: 6543/' ~/webview/src/scripts/settings.js
 sudo sed -i 's/archive.cnx.org/localhost:6543/' /etc/nginx/sites-available/webview
-./bin/fab -H localhost webview_run
 
 # Link webview to local accounts
 sed -i "s%accountProfile: .*%accountProfile: 'https://$ipaddr:3000/profile'%" ~/webview/src/scripts/settings.js
+
+./bin/fab -H localhost webview_run
 
 # Set up Connexions/cnx-publishing
 ./bin/fab -H localhost publishing_setup:https=True

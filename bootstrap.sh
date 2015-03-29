1291cdda850ccdb747a4b0bfaba9da9919a8ba35
karen chan <karen@karen-chan.com>
2015-03-29 18:34:32 +0100

    Update account profile setting in webview settings.js
    settings.js has been updated and now it requires a comma at the end of
    the account profile setting.

--- bootstrap.sh
+++ bootstrap.sh
@@ -78,7 +78,7 @@ sed -i 's/port: 80$/port: 6543/' ~/webview/src/scripts/settings.js
 sudo sed -i 's/archive.cnx.org/localhost:6543/' /etc/nginx/sites-available/webview
 
 # Link webview to local accounts
-sed -i "s%accountProfile: .*%accountProfile: 'https://$ipaddr:3000/profile'%" ~/webview/src/scripts/settings.js
+sed -i "s%accountProfile: .*%accountProfile: 'https://$ipaddr:3000/profile',%" ~/webview/src/scripts/settings.js
 
 ./bin/fab -H localhost webview_run
 

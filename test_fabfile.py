f8de39fae2d0f0e6add9e261c6bd66d763330a78
rich-hart <rich.hart@rice.edu>
2015-05-12 11:45:15 -0500

    adding base unittests

---
+++ test_fabfile.py
@@ -0,0 +1,18 @@
+import unittest
+from fabfile import *
+
+class TestCNXVagrant(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_empty(self):
+        pass
+
+#if __name__ == '__main__':
+#    unittest.main()
+
